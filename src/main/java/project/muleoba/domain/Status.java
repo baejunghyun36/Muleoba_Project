1247f591fc70540bbd5ba44b77e826dfb605d5ff
jikimomo <jikimomo@gmail.com>
2022-05-27 17:20:56 +0900

    초기 도메인

---
+++ src/main/java/project/muleoba/domain/Status.java
@@ -0,0 +1,5 @@
+package project.muleoba.domain;
+
+public enum Status {
+    Normal, Reservation, Complete
+}
---
+++ src/main/java/project/muleoba/domain/Transaction.java
@@ -0,0 +1,35 @@
+package project.muleoba.domain;
+
+import com.sun.istack.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long tID;
+
+    @NotNull
+    private Long requestIID;
+
+    @NotNull
+    private LocalDateTime requestTime;
+
+    @NotNull
+    private Status status;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="iID")
+    private Item item;
+
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "aID")
+    private Alarm alarm; //배송정보
+}
