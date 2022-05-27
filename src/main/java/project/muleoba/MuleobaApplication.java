6133ee3116fce4723171a990073e899cfe2f6674
baejunghyun36 <bae1004kin@gmail.com>
2022-05-27 15:49:18 +0900

    .

---
+++ src/main/java/project/muleoba/MuleobaApplication.java
@@ -0,0 +1,13 @@
+package project.muleoba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuleobaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuleobaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project/muleoba/MuleobaApplicationTests.java
@@ -0,0 +1,13 @@
+package project.muleoba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuleobaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
