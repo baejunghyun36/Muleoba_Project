00f6f26c51c5f1e0f199f3ead3313c2c6a368f29
baejunghyun36 <bae1004kin@gmail.com>
2022-05-30 11:17:39 +0900

    초기 세팅

---
+++ src/main/java/project/muleoba/service/alarmService/AlarmService.java
@@ -0,0 +1,4 @@
+package project.muleoba.service.alarmService;
+
+public interface AlarmService {
+}
---
+++ src/main/java/project/muleoba/service/userService/UserServiceImpl.java
@@ -0,0 +1,4 @@
+package project.muleoba.service.userService;
+
+public class UserServiceImpl {
+}
