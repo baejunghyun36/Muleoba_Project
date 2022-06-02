9f4d2fcade8b6a93f4bf17df21505e1daa69c301
yeeun <dpdms9621@naver.com>
2022-06-02 14:43:35 +0900

    .

--- src/main/java/project/muleoba/controller/UserController.java
+++ src/main/java/project/muleoba/controller/UserController.java
@@ -2,14 +2,18 @@ package project.muleoba.controller;
 
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import project.muleoba.domain.User;
 import project.muleoba.service.userService.UserService;
+import project.muleoba.vo.UserVO;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class UserController {
 
     private final UserService userService;
@@ -27,6 +31,21 @@ public class UserController {
     @PostMapping("/22") // 닉네임 중복여부 [중복없음:true]
     public boolean findNickNameUser(String nickName) {
         return userService.findNickNameUser(nickName);
+    }
+
+    @GetMapping("/muleoba/user")
+    public UserVO findUserUID(Long uID) {
+        log.info("이쪽이다{}", uID);
+        User user = userService.findByuID(uID);
+        UserVO uservo = new UserVO();
+        uservo.setUID(user.getUID());
+        uservo.setEmail(user.getEmail());
+        uservo.setNickName(user.getNickName());
+        uservo.setPhoneNumber(user.getPhoneNumber());
+        uservo.setName(user.getName());
+        uservo.setPassword(user.getPassword());
+        uservo.setAddress(user.getAddress());
+        return uservo;
     }
 
 
---
+++ src/main/java/project/muleoba/vo/UserVO.java
@@ -0,0 +1,17 @@
+package project.muleoba.vo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserVO {
+
+    private Long uID;
+    private String email;
+    private String nickName;
+    private String phoneNumber;
+    private String name;
+    private String password;
+    private String address;
+}
