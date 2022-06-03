5dd7de863701eee7e832e84b93a36112ec7c1419
baejunghyun36 <bae1004kin@gmail.com>
2022-06-03 17:47:24 +0900

    normal 일때만 출력

--- src/main/java/project/muleoba/repository/TransactionRepository.java
+++ src/main/java/project/muleoba/repository/TransactionRepository.java
@@ -18,6 +18,6 @@ public interface TransactionRepository extends JpaRepository<Transaction, Long> 
     @Query("select t from Transaction t where t.item.iID = :iid and t.requestIID = :requestIID")
     Transaction findByiidRequestIID(@Param("iid") Long iid,@Param("requestIID") Long requestIID);
 
-    @Query("select distinct t from Transaction  t, Item i where i.user.uID=:uid and t.requestIID = i.iID and t.status = 0 order by t.requestTime desc")
+    @Query("select distinct t from Transaction  t, Item i where i.user.uID=:uid and t.requestIID = i.iID and t.status = 'Normal' order by t.requestTime desc")
     List<Transaction> findAllDateDesc(@Param("uid") Long uid);
 }
