e827f07aabf5c71b859e866ed6cddb8ebe433c70
angiekimm <eungg22@gmail.com>
2022-06-04 20:00:35 +0900

    .

--- src/main/java/project/muleoba/controller/UserController.java
+++ src/main/java/project/muleoba/controller/UserController.java
@@ -2,6 +2,8 @@ package project.muleoba.controller;
 
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import project.muleoba.domain.User;
@@ -11,13 +13,14 @@ import project.muleoba.vo.UserVO;
 import java.util.List;
 
 @RestController
+@Slf4j
 @RequiredArgsConstructor
 public class UserController {
 
     private final UserService userService;
 
     @PostMapping("/muleoba/signup") // 회원가입 [ 로그인성공 : 1, 로그인실패 : 0 ]
-    public int saveUser(UserVO vo) {
+    public int saveUser(@RequestBody UserVO vo) {
         User user = new User();
         user.setEmail(vo.getEmail());
         user.setNickName(vo.getNickName());
@@ -28,17 +31,17 @@ public class UserController {
         return userService.saveUser(user);
     }
 
-    @PostMapping("/check/email") // 이메일 중복여부 [중복없음:true]
+    @GetMapping("/muleoba/check/email") // 이메일 중복여부 [중복없음:true]
     public boolean findEmailUser(String email){
        return userService.findEmailUser(email);
     }
 
-    @PostMapping("/check/nickname") // 닉네임 중복여부 [중복없음:true]
+    @GetMapping("/muleoba/check/nickname") // 닉네임 중복여부 [중복없음:true]
     public boolean findNickNameUser(String nickName) {
         return userService.findNickNameUser(nickName);
     }
 
-    @PostMapping("/login") // 로그인
+    @PostMapping("/muleoba/login") // 로그인
     public Long userLogin(String email, String password) {
         return userService.loginUser(email, password);
     }
