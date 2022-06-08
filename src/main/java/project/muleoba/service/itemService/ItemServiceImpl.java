fdb9ede502bbc39eda2b8b202845878869178861
angiekimm <eungg22@gmail.com>
2022-06-08 09:56:20 +0900

    .

--- src/main/java/project/muleoba/service/itemService/ItemServiceImpl.java
+++ src/main/java/project/muleoba/service/itemService/ItemServiceImpl.java
@@ -76,7 +76,7 @@ public class ItemServiceImpl implements  ItemService{
         if(!CollectionUtils.isEmpty(images)){ //이미지 파일이 존재할 경우
             //프로젝트 내의 static 폴더까지의 절대 경로
 //            String absolutePath = new File("").getAbsolutePath()+File.separator+"src"+File.separator+"main"+File.separator+"resources"+File.separator+"static";
-            String absolutePath ="C:\\Users\\bae10\\Desktop\\muleoba_front\\Muleoba_Front\\public";
+            String absolutePath ="/Users/angiekim/Documents/multicampus/3rd project/workspace/Muleoba_Front/public";
 
             for(MultipartFile image : images){
                 String originalFileExtension = new String();
@@ -97,7 +97,7 @@ public class ItemServiceImpl implements  ItemService{
                 String newFileName = System.nanoTime()+originalFileExtension; //이미지 이름이 겹치지 않게 나노시간을 이름으로 사진 저장
 //                File file = new File(absolutePath+File.separator+"itemImage"+File.separator+newFileName);
                 File file = new File(absolutePath+File.separator+"img"+File.separator+newFileName);
-                log.info("여기 2 {}",absolutePath+File.separator+"itemImage"+File.separator+newFileName );
+
 
                 image.transferTo(file);
                 file.setWritable(true);
@@ -105,7 +105,7 @@ public class ItemServiceImpl implements  ItemService{
 
 
 
-                String absolutePath2 = "C:\\Users\\bae10\\Desktop\\muleoba_front\\Muleoba_Front\\public\\img\\";
+                String absolutePath2 = "/Users/angiekim/Documents/multicampus/3rd project/workspace/Muleoba_Front/public/img/";
 
 
               /*  String absolutePath2 = new File("C:\\Users\\bae10\\Desktop\\muleoba_front\\Muleoba_Front\\public\\img").getPath();*/
